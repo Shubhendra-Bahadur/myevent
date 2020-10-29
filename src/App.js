afd8b415be0849a1f3790b0e05c2622cc90cfc35
Shubhendra-Bahadur <40620602+Shubhendra-Bahadur@users.noreply.github.com>
2020-10-30 01:13:02 +0530

    input field added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import './App.css';
+import { Button, FormControl, Input, InputLabel } from '@material-ui/core'
 
 function App() {
 
-  const [todos, setTodos] = useState(['microprocessor', 'analog']);
+  const [todos, setTodos] = useState([]);
   const [input, setInput] = useState('')
   // console.log(input);
 
@@ -15,9 +16,21 @@ function App() {
   return (
     <div className="App">
       <h1>My Event List !</h1>
-      <form>
+      {/* <form>
         <input type="text" value={input} onChange={event => setInput(event.target.value)} />
+        <Button disabled={!input} type='submit' onClick={addtodo} variant="contained" color="primary">
+          Add Todo
+        </Button>
         <button type='submit' onClick={addtodo}>Add Todo</button>
+      </form> */}
+      <form>
+        <FormControl onSubmit={addtodo}>
+          <InputLabel><i className="fas fa-check font-awsome-success"></i> Write Todo</InputLabel>
+          <Input type="text" value={input} onChange={event => setInput(event.target.value)}></Input>
+        </FormControl>
+        <Button disabled={!input} type='submit' onClick={addtodo} variant="contained" color="primary">
+          Add Todo
+      </Button>
       </form>
 
       <ul>
